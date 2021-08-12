4c2fea2c24e610ff6ef6964fdbe0abd09f23bee4
H. Xenokrates <Xenokrates@users.noreply.github.com>
2021-08-12 13:40:32 +0200

    Apply 'JavaScript Standard Style' and enforce it

--- nodes/find-group.js
+++ nodes/find-group.js
@@ -1,69 +1,66 @@
-module.exports = function(RED) {
-
-  function findGroupNode(config) {
-    RED.nodes.createNode(this,config);
-    var node = this;
+module.exports = function (RED) {
+  function findGroupNode (config) {
+    RED.nodes.createNode(this, config)
+    const node = this
     // we get the properties
-    node.url = config.url;
+    node.url = config.url
     node.baseDN = config.baseDN
     // we get the credentials
-    var cUsername = this.credentials.username;
-    var cPassword = this.credentials.password;
-    node.on('input', function(msg) {
-      node.status({fill:"blue",shape:"ring",text:"connecting"});
+    const cUsername = this.credentials.username
+    const cPassword = this.credentials.password
+    node.on('input', function (msg) {
+      node.status({ fill: 'blue', shape: 'ring', text: 'connecting' })
       // import activedirectory2
-      var ActiveDirectory = require('activedirectory2');
-      var adConfig = {
+      const ActiveDirectory = require('activedirectory2')
+      const adConfig = {
         url: node.url,
         baseDN: node.baseDN,
         username: cUsername,
         password: cPassword
-      };
+      }
       // set attributes if defined
       if (msg.ad_attributes) {
         // Validates the Object format (required for IBMi platform)
-        adConfig.attributes = JSON.parse(JSON.stringify(msg.ad_attributes));
+        adConfig.attributes = JSON.parse(JSON.stringify(msg.ad_attributes))
       }
       if (msg.tlsOptions) {
         // Validates the Object format (required for IBMi platform)
-        adConfig.tlsOptions = JSON.parse(JSON.stringify(msg.tlsOptions));
+        adConfig.tlsOptions = JSON.parse(JSON.stringify(msg.tlsOptions))
       }
       try {
-        var ad = new ActiveDirectory(adConfig);
-        node.status({fill:"green",shape:"dot",text:"connected"});
+        const ad = new ActiveDirectory(adConfig)
+        node.status({ fill: 'green', shape: 'dot', text: 'connected' })
         // Find group by his DN
-        var dn = msg.payload;
-        node.status({fill:"blue",shape:"ring",text:"querying"});
-        ad.findGroup(dn, function(err, group) {
+        const dn = msg.payload
+        node.status({ fill: 'blue', shape: 'ring', text: 'querying' })
+        ad.findGroup(dn, function (err, group) {
           if (err) {
-            let errTxt = 'ERROR querying: ' + JSON.stringify(err);
-            node.status({fill:"red", shape:"dot", text: errTxt});
-            node.error(errTxt, msg);
-          } else if (! group) {
-            let errTxt =  'Group ' + dn + ' not found.';
-            delete msg.payload;
-            msg.ad_error = errTxt;
-            node.status({fill:"yellow", shape:"dot", text: errTxt});
-            node.send(msg);
-          }else {
-            msg.payload = group;
-            node.status({fill:"green", shape:"dot", text: 'Group ' + dn + ' found'});
-            node.send(msg);
+            const errTxt = 'ERROR querying: ' + JSON.stringify(err)
+            node.status({ fill: 'red', shape: 'dot', text: errTxt })
+            node.error(errTxt, msg)
+          } else if (!group) {
+            const errTxt = 'Group ' + dn + ' not found.'
+            delete msg.payload
+            msg.ad_error = errTxt
+            node.status({ fill: 'yellow', shape: 'dot', text: errTxt })
+            node.send(msg)
+          } else {
+            msg.payload = group
+            node.status({ fill: 'green', shape: 'dot', text: 'Group ' + dn + ' found' })
+            node.send(msg)
           }
-        });
-      } catch(e) {
-        node.status({fill:"red", shape:"dot", text:"connection error"});
-        node.error('ERROR connecting: ' + e.message, msg);
+        })
+      } catch (e) {
+        node.status({ fill: 'red', shape: 'dot', text: 'connection error' })
+        node.error('ERROR connecting: ' + e.message, msg)
       }
-    });
+    })
   }
 
-  RED.nodes.registerType("find-group",findGroupNode,{
+  RED.nodes.registerType('find-group', findGroupNode, {
     credentials: {
-      username: {type:"text"},
-      password: {type:"password"}
+      username: { type: 'text' },
+      password: { type: 'password' }
     }
-  });
-  
-  }
-  
+  })
+}
--- nodes/find-user.js
+++ nodes/find-user.js
@@ -1,68 +1,67 @@
-module.exports = function(RED) {
-
-  function findUserNode(config) {
-    RED.nodes.createNode(this,config);
-    let node = this;
+module.exports = function (RED) {
+  function findUserNode (config) {
+    RED.nodes.createNode(this, config)
+    const node = this
     // we get the properties
-    node.url = config.url;
+    node.url = config.url
     node.baseDN = config.baseDN
     // we get the credentials
-    let cUsername = this.credentials.username;
-    let cPassword = this.credentials.password;
-    this.on('input', function(msg) {
-      node.status({fill:"blue",shape:"ring",text:"connecting"});
+    const cUsername = this.credentials.username
+    const cPassword = this.credentials.password
+    this.on('input', function (msg) {
+      node.status({ fill: 'blue', shape: 'ring', text: 'connecting' })
       // import activedirectory2
-      var ActiveDirectory = require('activedirectory2');
-      var adConfig = {
+      const ActiveDirectory = require('activedirectory2')
+      const adConfig = {
         url: node.url,
         baseDN: node.baseDN,
         username: cUsername,
         password: cPassword
-      };
+      }
       // set attributes if defined
       if (msg.ad_attributes) {
         // Validates the Object format (required for IBMi platform)
-        adConfig.attributes = JSON.parse(JSON.stringify(msg.ad_attributes));
+        adConfig.attributes = JSON.parse(JSON.stringify(msg.ad_attributes))
       }
       if (msg.tlsOptions) {
         // Validates the Object format (required for IBMi platform)
-        adConfig.tlsOptions = JSON.parse(JSON.stringify(msg.tlsOptions));
+        adConfig.tlsOptions = JSON.parse(JSON.stringify(msg.tlsOptions))
       }
       try {
-        var ad = new ActiveDirectory(adConfig);
-        node.status({fill:"green",shape:"dot",text:"connected"});
+        const ad = new ActiveDirectory(adConfig)
+        node.status({ fill: 'green', shape: 'dot', text: 'connected' })
         // Find user by his DN
-        var dn = msg.payload;
-        node.status({fill:"blue",shape:"ring",text:"querying"});
-        ad.findUser(dn, function(err, user) {
+        const dn = msg.payload
+        node.status({ fill: 'blue', shape: 'ring', text: 'querying' })
+        ad.findUser(dn, function (err, user) {
           if (err) {
-            let errTxt = 'ERROR querying: ' + JSON.stringify(err);
-            node.status({fill:"red", shape:"dot", text:"error querying"});
-            node.error(errTxt);
-          } else if (! user) {
-            var errTxt = "User " + dn + " not found";
-            delete msg.payload;
-            msg.ad_error = errTxt;
-            node.status({fill:"yellow", shape:"dot", text: errTxt});
-            node.send(msg);
+            const errTxt = 'ERROR querying: ' + JSON.stringify(err)
+            node.status({ fill: 'red', shape: 'dot', text: 'error querying' })
+            node.error(errTxt)
+          } else if (!user) {
+            const errTxt = 'User ' + dn + ' not found'
+            delete msg.payload
+            msg.ad_error = errTxt
+            node.status({ fill: 'yellow', shape: 'dot', text: errTxt })
+            node.send(msg)
           } else {
-            msg.payload = user;
-            node.status({fill:"green", shape:"dot", text:"user" + dn + "found"});
-            node.send(msg);
+            msg.payload = user
+            node.status({ fill: 'green', shape: 'dot', text: 'user' + dn + 'found' })
+            node.send(msg)
           }
-        });
-      } catch(e) {
-        let errTxt = 'ERROR connecting: ' + e.message;
-        node.status({fill:"red", shape:"dot", text:"connection error"});
-        node.error(errTxt, msg);
+        })
+      } catch (e) {
+        const errTxt = 'ERROR connecting: ' + e.message
+        node.status({ fill: 'red', shape: 'dot', text: 'connection error' })
+        node.error(errTxt, msg)
       }
-    });
+    })
   }
 
-  RED.nodes.registerType("find-user",findUserNode,{
+  RED.nodes.registerType('find-user', findUserNode, {
     credentials: {
-      username: {type:"text"},
-      password: {type:"password"}
+      username: { type: 'text' },
+      password: { type: 'password' }
     }
-  });
+  })
 }
